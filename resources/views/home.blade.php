564e8bde30423de32a918161ed75a552bceb2d01
Ibrahim <ibrahimpro.dev@gmail.com>
2023-11-26 23:23:23 +0300

    edited the home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -276,10 +276,10 @@
         <footer class="bg-dark py-4 mt-auto">
             <div class="container px-5">
                 <div class="row align-items-center justify-content-between flex-column flex-sm-row">
-                    <div class="col-auto"><div class="small m-0 text-white">Copyright &copy; Healthymind 2022</div></div>
+                    <div class="col-auto"><div class="small m-0 text-white">Copyright &copy; Healthyway 2023</div></div>
                     <div class="col-auto">
                         <a class="link-light small" href="{{URL('/')}}">Home</a>
-                        <span class="text-white mx-1">&middot;</span>
+                        <!-- <span class="text-white mx-1">&middot;</span> -->
                         <!-- <a class="link-light small" href="{{URL('forum')}}">Forum</a>
                         <span class="text-white mx-1">&middot;</span>
                         <a class="link-light small" href="/blog">Blog</a> -->
